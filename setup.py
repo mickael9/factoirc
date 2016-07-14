ca6ca4087229878e2616729f0aff80c56ce392a1
Mickaël Thomas <mickael9@gmail.com>
2016-07-14 22:34:04 +0200

    Release 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 setup(
     name='factoirc',
 
-    version='0.2',
+    version='0.3',
 
     description='Bidirectional IRC bridge for Factorio',
     long_description=long_description,
